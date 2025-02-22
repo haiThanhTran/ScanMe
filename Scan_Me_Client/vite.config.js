dc65e45ab26ecee719e8f82ed689268537fba063
Trần Thanh Hải <96519556+haiThanhTran@users.noreply.github.com>
2025-02-22 13:59:16 +0700

    fix deploy site

--- Scan_Me_Client/vite.config.js
+++ Scan_Me_Client/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: "~", replacement: "/src" }
-    ]
+    alias: [{ find: "~", replacement: "/src" }],
   },
   esbuild: {
     loader: "jsx",
@@ -15,7 +13,24 @@ export default defineConfig({
   },
   css: {
     modules: {
-      localsConvention: 'camelCase', // Tuỳ chọn: Cấu hình kiểu đặt tên class
+      localsConvention: "camelCase", // Tuỳ chọn: Cấu hình kiểu đặt tên class
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Đơn vị: kB
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
